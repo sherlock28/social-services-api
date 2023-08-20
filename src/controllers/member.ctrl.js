1a4de4497ccf57446617ebe0b5237d472ecd8254
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2023-08-20 10:50:59 -0300

    Adding create member method

--- src/controllers/member.ctrl.js
+++ src/controllers/member.ctrl.js
@@ -1,31 +1,60 @@
 import { HttpStatusCode } from '../const/statusCodes.js';
 import { serviceResponse } from "../libs/serviceResponse.js";
+import { PrismaClient } from "@prisma/client";
+import logger from "../config/logger.js";
+
+const prisma = new PrismaClient();
 
 class MemberController {
 
-	create(req, res) {
+	async create(req, res) {
 
-        return res.status(HttpStatusCode.OK).json(serviceResponse({ data: true, success: true, message: "Member created successfully", error: null }));
+		const { number, name, phone_one, phone_two, address, district, payday, last_pay_amount, plan_id, payments } = req.body;
+
+		try {
+			const newMember = await prisma.Member.create({
+				data: {
+					number,
+					name,
+					phone_one,
+					phone_two: phone_two ?? null,
+					address,
+					district,
+					payday,
+					last_pay_amount,
+					payments,
+					plan: {
+						connect: { id: plan_id }
+					}
+				}
+			});
+
+			return res.status(HttpStatusCode.OK).json(serviceResponse({ data: newMember, success: true, message: "Member created successfully", error: null }));
+
+		} catch (err) {
+			logger.error("Couldn't save member", err);
+			return res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json(serviceResponse({ data: null, success: true, message: "Couldn't save member", error: err }));
+		}
 	}
 
 	update(req, res) {
 
-        return res.status(HttpStatusCode.OK).json(serviceResponse({ data: true, success: true, message: "Member updated successfully", error: null }));
+		return res.status(HttpStatusCode.OK).json(serviceResponse({ data: true, success: true, message: "Member updated successfully", error: null }));
 	}
 
 	delete(req, res) {
 
-        return res.status(HttpStatusCode.OK).json(serviceResponse({ data: true, success: true, message: "Member deleted successfully", error: null }));
+		return res.status(HttpStatusCode.OK).json(serviceResponse({ data: true, success: true, message: "Member deleted successfully", error: null }));
 	}
 
 	getByNumber(req, res) {
 
-        return res.status(HttpStatusCode.OK).json(serviceResponse({ data: true, success: true, message: "Successfully obtained member", error: null }));
+		return res.status(HttpStatusCode.OK).json(serviceResponse({ data: true, success: true, message: "Successfully obtained member", error: null }));
 	}
 
 	get(req, res) {
 
-        return res.status(HttpStatusCode.OK).json(serviceResponse({ data: true, success: true, message: "Successfully obtained members", error: null }));
+		return res.status(HttpStatusCode.OK).json(serviceResponse({ data: true, success: true, message: "Successfully obtained members", error: null }));
 	}
 }
 
