a0fd9b5f7c85c4e0a0aa5e60f537809a79db9fe6
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2023-08-20 02:55:27 -0300

    Adding index server

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import express from 'express';
+import configureApp from './config/app.js';
+import logger from './config/logger.js';
 
-const app = express();
-
-app.get('/', (req, res) => {
-    res.json({
-        message: 'Hello, World!'
-    });
-});
-
-app.set('port', 3000);
+const app = configureApp(express());
 
 app.listen(app.get('port'), () => {
-    console.log(`Server listening on port ${app.get('port')}`);
+    logger.info('Server Running');
+    logger.info('Node Environment: ' + process.env.NODE_ENV);
+    if (process.env.NODE_ENV === "development") {
+        logger.info(`Local:            http://localhost:${process.env.PORT}`);
+        logger.info(`On Your Network:  http://${process.env.APP_DOMAIN}`);
+    }
 });
