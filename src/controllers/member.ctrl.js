f5a0342b2732762610a57254966a9773b6670c8d
Rodolfo Caceres <caceresrodolfo28@gmail.com>
2023-08-20 03:38:35 -0300

    Adding methods for CRUD of Member and routes

--- src/controllers/member.ctrl.js
+++ src/controllers/member.ctrl.js
@@ -7,6 +7,26 @@ class MemberController {
 
         return res.status(HttpStatusCode.OK).json(serviceResponse({ data: true, success: true, message: "Member created successfully", error: null }));
 	}
+
+	update(req, res) {
+
+        return res.status(HttpStatusCode.OK).json(serviceResponse({ data: true, success: true, message: "Member updated successfully", error: null }));
+	}
+
+	delete(req, res) {
+
+        return res.status(HttpStatusCode.OK).json(serviceResponse({ data: true, success: true, message: "Member deleted successfully", error: null }));
+	}
+
+	getByNumber(req, res) {
+
+        return res.status(HttpStatusCode.OK).json(serviceResponse({ data: true, success: true, message: "Successfully obtained member", error: null }));
+	}
+
+	get(req, res) {
+
+        return res.status(HttpStatusCode.OK).json(serviceResponse({ data: true, success: true, message: "Successfully obtained members", error: null }));
+	}
 }
 
 export const memberCtrl = new MemberController();
--- src/routes/member.route.js
+++ src/routes/member.route.js
@@ -5,5 +5,9 @@ import { validateCreate } from '../validators/member.js';
 const router = Router();
 
 router.post('/', validateCreate, memberCtrl.create);
+router.put('/:number', memberCtrl.update);
+router.delete('/:number', memberCtrl.delete);
+router.get('/:number', memberCtrl.getByNumber);
+router.get('/', memberCtrl.get);
 
 export default router;
